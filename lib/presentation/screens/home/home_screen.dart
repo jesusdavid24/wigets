ac5cf96592e360848a25361b04c80eed6bbbb09d
Jesus Bravo <jd_bravo24@hotmail.com>
2024-02-02 07:13:05 -0500

    list view

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,25 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + material 3'),
       ),
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: appMenuItems.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItems[index];
+          return SizedBox(
+            height: 50,
+            child: Text(menuItem.title),
+          );
+        });
   }
 }
